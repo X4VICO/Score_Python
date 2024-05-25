0c4707288f6a1f7db9c5d8ca8018425d75758b54
X4VICO <156704558+X4VICO@users.noreply.github.com>
2024-05-25 14:53:39 +0200

    Update scorelib.py

--- scorelib.py
+++ scorelib.py
@@ -1,61 +1,58 @@
-# LIBRERIA PRINCIPAL
+# LIBRERÍA PRINCIPAL
 
-# creamos un objeto
 class Score:
-    # iniciamos y escribimos el arrray que usaremos para las posiciones
-    def __init__(self, scorefile='score.txt'):
-        self.scorefile = scorefile
+    # ----- función inicial de la clase "Score" -----
+    def __init__(self, scorefile='score.txt'): # aseguramos de nuevo que el valor por defecto de "scorefile" sea "score.txt"
+        self.scorefile = scorefile # guardamos el valor obtenido en la variable de la instancia
+        # creamos una lista de las 20 posiciones posibles que tendrá el archivo de puntuaciones
         self.posiciones = [" 1ST", " 2ND", " 3RD", " 4TH", " 5TH", " 6TH", " 7TH", " 8TH", " 9TH", "10TH", "11TH", "12TH", "13TH", "14TH", "15TH", "16TH", "17TH", "18TH", "19TH", "20TH"]
     
-# ------función para añadir puntos------
-    def add_score(self, points, name, scorefile=None):
-        # verificamos que el numero intrducido esta dentro los parametros correctos
-        if not (0 <= points <= 99999):
-            print("Error al registrar puntos.")
-        # verificamos que el nombre tiene 3 caracteres
-        if len(name) != 3:
-            print("Error al registrar créditos.")
-            return 
+    # ----- función añadir resultados -----
+    def add_score(self, points, name, scorefile=None): # aplicamos "scorefile=None" por si no hemos obtenido ningún argumento de archivo, se use el valor por defecto "score.txt"
+        if not (0 <= points <= 99999): # si el valor de "points" no está dentro del rango especifico...
+            print("Error al registrar puntos.") # ... informamos del error...
+            return # ... seguidamente salimos de la función finalizando el script
+        if len(name) != 3: # si no se introduce un nombre con una longitud de 3 caracteres alfanuméricos...
+            print("Error al registrar créditos.") # ... informamos del error...
+            return # ... seguidamente salimos de la función finalizando el script
                 
-        name = name.upper() # nombre en mayusculas
-        new_score = (points, name) # creamos la variable new_score
-        
-        # nos aseguramos que usa el archivo de puntuacion correcto
-        if scorefile is None:
+        name = name.upper() # pasamos los caracteres introducidos en el nombre a mayúsculas
+        new_score = (points, name) # guardamos los valores nuevos en una variable llamada "new_score"
+
+        # como tuvimos problemas con "scorefile", en el caso de no obtener ningún argumento... 
+        if scorefile is None: # ... decidimos, de nuevo, asegurarnos de que la variable use el valor por defecto "score.txt"
             scorefile = self.scorefile
-
-        scores = self.get_scores(scorefile)
         
-        # si la nueva puntuacion no entra en el top 20 y los puntos introducidos son menores que el 20TH actual... 
-        if len(scores) >= 20 and points <= scores[-1][0]:
-            print("Vuelve a jugar para mejorar tu puntuación.") # ...muestra esto
-            return
-
-        # añadimos la nueva puntuacion y ordenamos la lista de puntuaciones y guarda solo las top 20
-        scores.append(new_score)
-        scores = sorted(scores, key=lambda x: -x[0])[:20]
-
-        # abrimos fichero en modo ecritura, obtiene los parametros... 
-        with open(scorefile, 'w') as fichero:
-            for i, (score_points, score_name) in enumerate(scores):
-                position = self.posiciones[i] # ...agrega la posicion... 
-                fichero.write(f"{position}     {str(score_points).zfill(5)}   {score_name}\n")   
-
-    # ------función para leer puntuaciones actuales------ 
-    def get_scores(self, scorefile=None):
-        # comprobamos que el fichero de scorefile es el correcto
-        if scorefile is None:
-            scorefile = self.scorefile # y si no hay ninguno especificado, usamos el score.txt de self
-        scores = [] # creamos una array vacia
+        scores = self.get_scores(scorefile) # llamamos a la función "get_scores", donde "scores" será una lista donde almacenar las puntuaciones
         
-        try: # si no hay archivo sale del bucle para evitar el error
-            # abre el archivo de puntuaciones en modo lectura
+        # ----- verificación resultados mostrados -----
+        # comprobamos que las puntuaciones nuevas estén dentro del limite del top 20. Si lo están, comprobamos que los puntos de estas nuevas puntuaciónes... 
+        if len(scores) >= 20 and points <= scores[-1][0]: # ... séan mayores a los de la ultima puntuación ([1] = ultimo del top 20) ([0] = argumento points) 
+            print("Vuelve a jugar para mejorar tu puntuación.") # en el caso de no estar dentro del top o tener menos puntos que el top 20, informamos de ello...
+            return # ... seguidamente salimos de la función finalizando el script
+
+        scores.append(new_score) # añadimos la nueva puntuación a la lista "scores"
+        scores = sorted(scores, key=lambda x: -x[0])[:20] # ordenamos los puntos ([0] = argumento points) de forma descendente (-x) y nos quedamos con los 20 primeros ([:20]) 
+
+        with open(scorefile, 'w') as fichero: # hacemos uso del "with open...as fichero" para que se cierre "scorefile" al salir del with
+            for i, (score_points, score_name) in enumerate(scores): # enumeramos y damos variables a los valores de cada puntuación que hay dentro de "scores"
+                position = self.posiciones[i] # asignamos a una variable nueva la posición correspondiente 
+                fichero.write(f"{position}     {str(score_points).zfill(5)}   {score_name}\n") # escribimos en el fichero la puntuacion con su formato correspondiente
+
+    # ----- función coger resultados -----
+    def get_scores(self, scorefile=None): 
+        if scorefile is None: # aseguramos de que si no se ha escogido archivo, se use el valor por defecto "score.txt"
+            scorefile = self.scorefile 
+        
+        scores = [] # creamos una lista vacía
+        
+        try: # usamos "try" por si nos da un error al abrir el archivo
             with open(scorefile, 'r') as fichero:
                 for linea in fichero:
-                    partes = linea.strip().split() # separa la linea por columna y por fila
-                    points = int(partes[1]) # convierte la puntuación a int
-                    name = partes[2] # asigna el 3r elemento de partes a name
-                    scores.append((points, name)) # y agrega puntuación, nombre a la lista de puntuaciones
+                    partes = linea.strip().split() # quitamos los espacios en blanco y dividimos la línea en partes
+                    points = int(partes[1]) # ([1] = puntuación) guardamos la primera partición en "points"  
+                    name = partes[2] # ([2] = nombre) guardamos la segunda partición en "name"  
+                    scores.append((points, name)) # lo añadimos a la lista "scores"
         except FileNotFoundError:
             pass
-        return scores
+        return scores # devolvemos el resultado de la lista obtenida en esta función
